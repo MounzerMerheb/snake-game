218dd4c986fef9cb2d2a6e14469432f597be2f1a
Hicham1S <sarajhicham@gmail.com>
2024-05-25 14:37:27 +0300

    getArr

--- snake.js
+++ snake.js
@@ -1,38 +1,47 @@
 class Node {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.next = null;
-  }
-  isNull() {
-    return x == -1 || y == -1;
-  }
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.next = null;
+    }
+    isNull() {
+        return x == -1 || y == -1;
+    }
 }
 class Snake {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 
-  addNode(x, y) {
-    let node = new Node(x, y);
-    if (this.head === null) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.head.next = node;
-      this.head = node;
+    addNode(x, y) {
+        let node = new Node(x, y);
+        if (this.head === null) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.head.next = node;
+            this.head = node;
+        }
+        this.length++;
     }
-  }
-  display() {
-    console.log("hello");
-    let temp = this.tail;
-    while (temp != null) {
-      console.log(temp.x, temp.y);
-      temp = temp.next;
+    display() {
+        console.log("hello");
+        let temp = this.tail;
+        while (temp != null) {
+            console.log(temp.x, temp.y);
+            temp = temp.next;
+        }
     }
-  }
+    getArr() {
+        let arr = [];
+        let temp = this.tail;
+        while (temp != null) {
+            arr.push(temp);
+            temp = temp.next;
+        }
+    }
 }
 
 export default Snake;
