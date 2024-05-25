973ab5807ad46fa4cd351478cafa6fc780776f72
Hicham1S <sarajhicham@gmail.com>
2024-05-25 13:59:00 +0300

    snake init

--- snake.js
+++ snake.js
@@ -1,4 +1,24 @@
-class Node {}
-class Snake {}
+class Node {
+    constructor(x,y){
+        this.x = x ;
+        this.y = y;
+        this.next
+    }
+}
+class Snake {
+    constructor(){
+
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+    
+    }
+
+    addNode(x,y){
+        let node = new Node(x,y);
+        this.head.next = node;
+        head = node;
+    }
+}
 
 export default Snake;
