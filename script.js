82d0b0d9a618fd681ad9b445f8bd986ca3a8e413
Mounzer El Merheb <mounzer.merheb.002@gmail.com>
2024-05-25 12:49:20 +0300

    grid created

---
+++ script.js
@@ -0,0 +1,27 @@
+const nbRows = 10;
+const nbCols = 10;
+const cellSize = 40;
+
+const grid = Array.from(Array(nbRows), () =>
+  Object.seal(new Array(nbCols).fill(0))
+);
+
+document.documentElement.style.setProperty("--cols", nbCols);
+document.documentElement.style.setProperty("--cellSize", cellSize + "px");
+
+const container = document.getElementById("grid-container");
+
+createGrid();
+
+function createGrid() {
+  for (let row = 0; row < nbRows; row++) {
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("grid-row");
+    for (let col = 0; col < nbCols; col++) {
+      const cellElement = document.createElement("div");
+      cellElement.classList.add("grid-cell");
+      rowElement.appendChild(cellElement);
+    }
+    container.appendChild(rowElement);
+  }
+}
