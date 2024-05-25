705ab1fbc431a79da6a99f44ce86c2b3434216ff
Mounzer El Merheb <mounzer.merheb.002@gmail.com>
2024-05-25 13:59:59 +0300

    add Apple

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const nbRows = 20;
 const nbCols = 20;
 const cellSize = 40;
 
+const values = {
+  1: "snake",
+  2: "apple",
+};
+
 const grid = Array.from(Array(nbRows), () =>
   Object.seal(new Array(nbCols).fill(0))
 );
@@ -13,6 +18,7 @@ document.documentElement.style.setProperty("--cellSize", cellSize + "px");
 const container = document.getElementById("grid-container");
 
 createGrid();
+placeApple();
 
 function createGrid() {
   for (let row = 0; row < nbRows; row++) {
@@ -25,4 +31,32 @@ function createGrid() {
     }
     container.appendChild(rowElement);
   }
+}
+
+function updateGrid(row, col, val) {
+  if (val <= 0) return;
+  grid[row][col] = val;
+  container.children[row].children[col].classList.add(values[val]);
+}
+
+function placeApple() {
+  let exclusion = [];
+  let rowApple, colApple;
+  do {
+    rowApple = random(0, nbRows);
+    colApple = random(0, nbCols);
+  } while (isArrayIncluded([rowApple, colApple], exclusion));
+  updateGrid(rowApple, colApple, 2);
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function isArrayIncluded(arr, arr2D) {
+  return arr2D.some(
+    (subArray) =>
+      subArray.length === arr.length &&
+      subArray.every((value, index) => value === arr[index])
+  );
 }
