8ed3168ce35dc72191948cdf3e7ef384aaac0a3c
Mounzer El Merheb <mounzer.merheb.002@gmail.com>
2024-05-27 12:23:50 +0300

    working game

--- script.js
+++ script.js
@@ -1,77 +1,263 @@
 import Snake from "./snake.js";
-const nbRows = 20;
-const nbCols = 20;
-const cellSize = 40;
+document.addEventListener("DOMContentLoaded", () => {
+  const nbRows = 15;
+  const nbCols = 25;
+  const cellSize = 30;
+  const values = {
+    1: "snake",
+    2: "apple",
+  };
+  const container = document.getElementById("grid-container");
 
-const values = {
-  1: "snake",
-  2: "apple",
-};
+  let snake;
+  let apple;
+  let direction;
+  let isAppleEaten;
 
-const grid = Array.from(Array(nbRows), () =>
-  Object.seal(new Array(nbCols).fill(0))
-);
+  let grid;
 
-document.documentElement.style.setProperty("--cols", nbCols);
-document.documentElement.style.setProperty("--cellSize", cellSize + "px");
+  document.documentElement.style.setProperty("--cols", nbCols);
+  document.documentElement.style.setProperty("--cellSize", cellSize + "px");
 
-const container = document.getElementById("grid-container");
+  initGame();
 
-createGrid();
-placeApple();
-let x = nbCols / 2;
-let y = nbRows / 2;
-let snake = new Snake();
-snake.addNode(x, y);
-snake.addNode(x + 1, y);
-snake.addNode(x + 2, y);
-updateSnake();
+  // ===============================================
+  let lastTime = 0;
+  let canChangeDirection = true;
+  let willLose = false;
 
-function createGrid() {
-  for (let row = 0; row < nbRows; row++) {
-    const rowElement = document.createElement("div");
-    rowElement.classList.add("grid-row");
-    for (let col = 0; col < nbCols; col++) {
-      const cellElement = document.createElement("div");
-      cellElement.classList.add("grid-cell");
-      rowElement.appendChild(cellElement);
+  function updateScreen(time) {
+    if (time - lastTime >= 200) {
+      move();
+      if (isLosing()) {
+        return;
+      }
+      eatApple();
+      canChangeDirection = true;
+      lastTime = time;
     }
-    container.appendChild(rowElement);
+
+    requestAnimationFrame(updateScreen);
   }
-}
+  document.addEventListener("keydown", handleKeyDown);
+  startGame();
 
-function updateGrid(row, col, val) {
-  if (val <= 0) return;
-  grid[row][col] = val;
-  container.children[row].children[col].classList.add(values[val]);
-}
+  // ===================================================
 
-function placeApple() {
-  let exclusion = [];
-  let rowApple, colApple;
-  do {
-    rowApple = random(0, nbRows - 1);
-    colApple = random(0, nbCols - 1);
-  } while (isArrayIncluded([rowApple, colApple], exclusion));
-  updateGrid(rowApple, colApple, 2);
-}
-
-function updateSnake() {
-  let temp = snake.tail;
-  while (temp != null) {
-    updateGrid(temp.y, temp.x, 1);
-    temp = temp.next;
+  function createGrid() {
+    for (let row = 0; row < nbRows; row++) {
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("grid-row");
+      for (let col = 0; col < nbCols; col++) {
+        const cellElement = document.createElement("div");
+        cellElement.classList.add("grid-cell");
+        rowElement.appendChild(cellElement);
+      }
+      container.appendChild(rowElement);
+    }
   }
-}
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
+  function startGame() {
+    lastTime = 0; // Reset lastTime if necessary
+    requestAnimationFrame(updateScreen);
+  }
+  function initGame() {
+    snake = new Snake();
+    apple = [];
+    direction = "L";
+    isAppleEaten = false;
 
-function isArrayIncluded(arr, arr2D) {
-  return arr2D.some(
-    (subArray) =>
-      subArray.length === arr.length &&
-      subArray.every((value, index) => value === arr[index])
-  );
-}
+    grid = Array.from(Array(nbRows), () =>
+      Object.seal(new Array(nbCols).fill(0))
+    );
+    createGrid();
+    initSnake(3);
+    placeSnake();
+    placeApple();
+  }
+
+  function restart() {
+    removeApple();
+    removeSnake();
+    snake = new Snake();
+    apple = [];
+    direction = "L";
+    isAppleEaten = false;
+    canChangeDirection = true;
+    willLose = false;
+    grid = Array.from(Array(nbRows), () =>
+      Object.seal(new Array(nbCols).fill(0))
+    );
+    initSnake(3);
+    placeSnake();
+    placeApple();
+    startGame();
+  }
+
+  function eatApple() {
+    //check collision
+    if (!(snake.head.y == apple[0] && snake.head.x == apple[1])) {
+      isAppleEaten = false;
+      return;
+    }
+    removeApple();
+
+    snake.addNode(snake.tail.x, snake.tail.y);
+
+    placeApple();
+    isAppleEaten = true;
+  }
+
+  function isLosing() {
+    return isLosingOnWall() || isLosingOnSelf();
+  }
+  function isLosingOnSelf() {
+    return isArrayIncluded(
+      [snake.head.y, snake.head.x],
+      snake.getPosArrButHead()
+    );
+  }
+  function isLosingOnWall() {
+    if (willLose) {
+      return snake.isOnEdge(direction, nbCols - 1, nbRows - 1);
+    }
+    willLose = snake.isOnEdge(direction, nbCols - 1, nbRows - 1);
+    return false;
+  }
+
+  function move() {
+    if (snake.isOnEdge(direction, nbCols - 1, nbRows - 1)) return;
+    if (!isAppleEaten) {
+      moveAll();
+    } else {
+      moveButTail();
+    }
+  }
+
+  function moveAll() {
+    removeSnake();
+    let moved = snake.move(direction, nbCols - 1, nbRows - 1);
+    placeSnake();
+    return moved;
+  }
+
+  function moveButTail() {
+    removeSnake();
+    let moved = snake.moveButTail(direction, nbCols, nbRows);
+    placeSnake();
+    return moved;
+  }
+
+  function updateGrid(row, col, val) {
+    if (val <= 0) return;
+
+    grid[row][col] = val;
+    container.children[row].children[col].classList.add(values[val]);
+  }
+
+  function placeApple() {
+    let exclusion = snake.getPosArr();
+    let rowApple, colApple;
+    do {
+      rowApple = random(0, nbRows);
+      colApple = random(0, nbCols);
+    } while (isArrayIncluded([rowApple, colApple], exclusion));
+    updateGrid(rowApple, colApple, 2);
+    apple = [rowApple, colApple];
+  }
+  function removeApple() {
+    removeFromGrid(apple[0], apple[1], 2);
+    apple = [];
+  }
+
+  function placeSnake() {
+    let temp = snake.tail;
+    while (temp != null) {
+      updateGrid(temp.y, temp.x, 1);
+      temp = temp.next;
+    }
+    container.children[snake.head.y].children[snake.head.x].classList.add(
+      "head"
+    );
+  }
+
+  function initSnake(length) {
+    let x = parseInt(nbCols / 2) + length - 1;
+    let y = parseInt(nbRows / 2);
+    for (let i = length - 1; i >= 0; i--) {
+      snake.addNode(x - i, y);
+    }
+  }
+
+  function removeFromGrid(row, col, val) {
+    grid[row][col] = 0;
+    container.children[row].children[col].classList.remove(values[val]);
+  }
+  function removeSnake() {
+    let temp = snake.tail;
+    while (temp != null) {
+      removeFromGrid(temp.y, temp.x, 1);
+      temp = temp.next;
+    }
+    container.children[snake.head.y].children[snake.head.x].classList.remove(
+      "head"
+    );
+  }
+
+  function handleKeyDown(event) {
+    if (!canChangeDirection) return;
+    const key = event.key; // The key value
+    const keyCode = event.keyCode; // The key code
+
+    // Perform actions based on the key pressed
+    switch (keyCode) {
+      case 37: // Left arrow key
+      case 65: // 'A' key
+        if (direction == "R" || direction == "L") {
+          return;
+        }
+        direction = "L";
+        canChangeDirection = false;
+        // move();
+
+        break;
+      case 38: // Up arrow key
+      case 87: // 'W' key
+        if (direction == "D" || direction == "U") return;
+        direction = "U";
+        canChangeDirection = false;
+        // move();
+
+        break;
+      case 39: // Right arrow key
+      case 68: // 'D' key
+        if (direction == "L" || direction == "R") return;
+        direction = "R";
+        canChangeDirection = false;
+        // move();
+        break;
+      case 40: // Down arrow key
+      case 83: // 'S' key
+        if (direction == "U" || direction == "D") return;
+        direction = "D";
+        canChangeDirection = false;
+        // move();
+        break;
+      default:
+        restart();
+        break;
+    }
+  }
+
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function isArrayIncluded(arr, arr2D) {
+    return arr2D.some(
+      (subArray) =>
+        subArray.length === arr.length &&
+        subArray.every((value, index) => value === arr[index])
+    );
+  }
+});
--- snake.js
+++ snake.js
@@ -1,47 +1,142 @@
 class Node {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.next = null;
-    }
-    isNull() {
-        return x == -1 || y == -1;
-    }
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.next = null;
+  }
+  isNull() {
+    return x == -1 || y == -1;
+  }
 }
 class Snake {
-    constructor() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  addNode(x, y) {
+    let node = new Node(x, y);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.tail;
+      this.tail = node;
+    }
+    this.length++;
+  }
+  display() {
+    console.log("hello");
+    let temp = this.tail;
+    while (temp != null) {
+      console.log(temp.x, temp.y);
+      temp = temp.next;
+    }
+  }
+  getPosArr() {
+    let arr = [];
+    let temp = this.tail;
+    while (temp != null) {
+      arr.push([temp.y, temp.x]);
+      temp = temp.next;
+    }
+    return arr;
+  }
+  getPosArrButHead() {
+    let arr = [];
+    let temp = this.tail;
+    while (temp != null) {
+      if (temp != this.head) arr.push([temp.y, temp.x]);
+      temp = temp.next;
+    }
+    return arr;
+  }
+
+  isOnEdge(direction, maxX, maxY) {
+    switch (direction) {
+      case "U":
+        if (this.head.y == 0) return true;
+
+        break;
+      case "D":
+        if (this.head.y == maxY) return true;
+
+        break;
+      case "L":
+        if (this.head.x == 0) return true;
+
+        break;
+      case "R":
+        if (this.head.x == maxX) return true;
+
+        break;
+      default:
+        return false;
+    }
+    return false;
+  }
+
+  moveButTail(direction, maxX, maxY) {
+    if (this.isOnEdge(direction, maxX, maxY)) return false;
+    let temp = this.tail.next;
+    while (temp != null) {
+      if (temp != this.head) {
+        temp.x = temp.next.x;
+        temp.y = temp.next.y;
+      }
+
+      temp = temp.next;
     }
 
-    addNode(x, y) {
-        let node = new Node(x, y);
-        if (this.head === null) {
-            this.head = node;
-            this.tail = node;
-        } else {
-            this.head.next = node;
-            this.head = node;
-        }
-        this.length++;
+    switch (direction) {
+      case "U":
+        this.head.y--;
+        break;
+      case "D":
+        this.head.y++;
+        break;
+      case "L":
+        this.head.x--;
+        break;
+      case "R":
+        this.head.x++;
+        break;
+      default:
+        break;
     }
-    display() {
-        console.log("hello");
-        let temp = this.tail;
-        while (temp != null) {
-            console.log(temp.x, temp.y);
-            temp = temp.next;
-        }
+    return false;
+  }
+  move(direction, maxX, maxY) {
+    if (this.isOnEdge(direction, maxX, maxY)) return false;
+    let temp = this.tail;
+    while (temp != null) {
+      if (temp != this.head) {
+        temp.x = temp.next.x;
+        temp.y = temp.next.y;
+      }
+
+      temp = temp.next;
     }
-    getArr() {
-        let arr = [];
-        let temp = this.tail;
-        while (temp != null) {
-            arr.push(temp);
-            temp = temp.next;
-        }
+
+    switch (direction) {
+      case "U":
+        this.head.y--;
+        break;
+      case "D":
+        this.head.y++;
+        break;
+      case "L":
+        this.head.x--;
+        break;
+      case "R":
+        this.head.x++;
+        break;
+      default:
+        break;
     }
+    return true;
+  }
 }
 
 export default Snake;
